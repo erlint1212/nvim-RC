9eab99a7d3002588dec1ec19e9f3aa18b210cbde
erlint1212 <e.t.nordtvedt@gmail.com>
2026-02-12 15:24:47 +0100

    Made all the spectre commands global

--- after/plugin/spectre.lua
+++ after/plugin/spectre.lua
@@ -1,31 +1,34 @@
 local spectre = require("spectre")
 
 spectre.setup({
-    -- You can add custom options here if needed,
-    -- otherwise the defaults are usually great.
     color_devicons = true,
     open_cmd = 'vnew',
-    live_update = true, -- Auto-update search results as you type
+    live_update = true,
+    -- Ensure the default is always project-wide
+    default = {
+        find = {
+            cmd = "rg",
+            options = {"--ignore-case"}
+        },
+        replace = {
+            cmd = "sed"
+        }
+    },
 })
 
--- Keymaps
+-- KEYMAPS (Project-Wide Only)
 
--- Open Spectre (Global Search)
-vim.keymap.set('n', '<leader>S', function() spectre.toggle() end, {
-    desc = "Toggle Spectre"
-})
+-- 1. Open Spectre (Clean state)
+vim.keymap.set('n', '<leader>S', function() 
+    spectre.toggle() 
+end, { desc = "Toggle Spectre (Project)" })
 
--- Search for the word under the cursor
-vim.keymap.set('n', '<leader>sw', function() spectre.open_visual({select_word=true}) end, {
-    desc = "Search current word"
-})
+-- 2. Search for the word under the cursor (Project-wide)
+vim.keymap.set('n', '<leader>sw', function() 
+    spectre.open_visual({select_word=true}) 
+end, { desc = "Search current word (Project)" })
 
--- Search for the currently selected text (Visual Mode)
-vim.keymap.set('v', '<leader>sw', function() spectre.open_visual() end, {
-    desc = "Search current selection"
-})
-
--- Search strictly within the current file
-vim.keymap.set('n', '<leader>sp', function() spectre.open_file_search({select_word=true}) end, {
-    desc = "Search on current file"
-})
+-- 3. Search for the currently selected text (Project-wide)
+vim.keymap.set('v', '<leader>sw', function() 
+    spectre.open_visual() 
+end, { desc = "Search selection (Project)" })
